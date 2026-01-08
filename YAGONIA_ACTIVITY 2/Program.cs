7b11d3003cbe7c262b84117c0bedbcfb991041ae
Jack Anthony Dolino Yagonia <jackyagonia9@gmail.com>
2026-01-08 16:27:53 +0800

    Add files via upload

---
+++ YAGONIA_ACTIVITY 2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YAGONIA_ACTIVITY_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Songs[] songs = new Songs[6];
+
+            //Values for Pop music
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            songs[0] = new PopMusic("Golden", "Jessa Esquirdo", 2025, "English");
+            songs[1] = new PopMusic("Fashion", "Cortis", 2025, "Korean");
+            songs[2] = new PopMusic("Despacito", "Luis Fonsi", 2016, "Spanish");
+            
+
+            //Values for Rock music
+            songs[3] = new RockMusic("Master of Puppets", "Metallica", 1986, "Electric Guitar");
+            songs[4] = new RockMusic("Given Up", "Linkin Park", 2008, "Bass Guitar");
+            songs[5] = new RockMusic("Highway to Hell", "AC/DC", 1979, "Drums");
+
+            //Repitition structure to display each song
+           foreach (var song in songs)
+            {
+                song.Display();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ YAGONIA_ACTIVITY 2/Songs.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YAGONIA_ACTIVITY_2
+{
+    internal class Songs
+    {
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public int Produced { get; set; }
+
+        //Parameterized Constructor
+        public Songs(string title, string artist, int produced)
+        {
+            Title = title;
+            Artist = artist;
+            Produced = produced;
+        }
+
+        public virtual void Display()
+        {
+            Console.WriteLine($"Title: {Title}");
+            Console.WriteLine($"Artist: {Artist}");
+            Console.WriteLine($"Produced: {Produced}");
+        }
+    }
+
+    class PopMusic : Songs //Derived Class
+    {
+        public string Language { get; set; }
+
+        //Parameterized Constructor
+        public PopMusic(string title, string artist, int produced, string language) : base(title, artist, produced)
+        {
+            Language = language;
+        }
+        public override void Display()
+        {
+            Console.WriteLine("\n---Pop Music Details---");
+            Console.WriteLine($"Pop Music Language: {Language}");
+            base.Display();
+        }
+    }
+    class RockMusic : Songs //Derived Class
+    {
+        public string Instruments { get; set; }
+
+        //Parameterized Constructor
+        public RockMusic(string title, string artist, int produced, string instruments) : base(title, artist, produced)
+        {
+            Instruments = instruments;
+        }
+        public override void Display()
+        {
+            Console.WriteLine("\n---Rock Music Details---");
+            Console.WriteLine($"Rock Music Instruments: {Instruments}");
+            base.Display();
+        }
+    }
+}
