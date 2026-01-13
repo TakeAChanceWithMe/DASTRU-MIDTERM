99fe353f26ed064456778310c134596238b11500
Jack Anthony Dolino Yagonia <jackyagonia9@gmail.com>
2026-01-13 17:15:29 +0800

    Add files via upload

---
+++ YAGONIA_ACTIVITY 3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YAGONIA_ACTIVITY_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creation of two-Dimensional Array for Songs objects
+            Songs[,] songs = new Songs[3,3];
+
+            //Values for Pop music
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            songs[0,0] = new PopMusic("Golden", "Jessa Esquirdo", 2025, "English");
+            songs[0,1] = new PopMusic("Fashion", "Cortis", 2025, "Korean");
+            songs[0,2] = new PopMusic("Despacito", "Luis Fonsi", 2016, "Spanish");
+            
+
+            //Values for Rock music
+            songs[1,0] = new RockMusic("Master of Puppets", "Metallica", 1986, "Electric Guitar");
+            songs[1,1] = new RockMusic("Given Up", "Linkin Park", 2008, "Bass Guitar");
+            songs[1,2] = new RockMusic("Highway to Hell", "AC/DC", 1979, "Drums");
+
+            //Repitition structure to display each song
+           for (int i = 0; i < songs.GetLength(0); i++)
+            {
+                for (int j = 0; j < songs.GetLength(1); j++)
+                {
+                    songs[i, j]?.DisplayInfo();
+                }
+            }
+           Console.ReadKey();
+
+        }
+    }
+}
---
+++ Yagonia_ACTIVITY3-2DArray/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yagonia_ACTIVITY3_2DArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            //This program demonstrates the use of a 2D array in C#
+            string[,] section = new string[3, 4]
+            {
+                {"\nRow 1\tIT401P - Yap", "\t\tIT402P - Juarez", "\t\tIT403P - Soberano", "\t\tIT404P - Minaj"},
+                {"Row 2\tMM401P - Pepito", "\tMM402P - Marquez", "\t\tMM403P - Gomez", "\t\t\tMM404P - Grande"},
+                {"Row 3\tTM101P - Salvo", "\t\tTM102P - Zefanya", "\t\tTM103P - Mendez", "\t\tTM104P - Pomoy"}
+            };
+
+            //Display sections
+            Console.WriteLine("Sections with the smartest students base on the course that they're in:\n");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("\t  Column 1:\t\t" + "   Column 2:\t\t\t" + "   Column 3:\t\t\t" + "   Column 4:\t");
+            Console.ForegroundColor = ConsoleColor.White;
+            for (int i = 0; i < section.GetLength(0); i++)
+            {
+                for(int j = 0; j < section.GetLength(1); j++)
+                {
+                    Console.Write(section[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
