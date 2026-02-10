ac8574050c38711c811b707c29b0f06af2063eb0
Jack Anthony Dolino Yagonia <jackyagonia9@gmail.com>
2026-02-10 20:56:57 +0800

    Add files via upload

---
+++ YAGONIA_ACTIVITY 8/YAGONIA_ACTIVITY 8/Songs.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YAGONIA_ACTIVITY_8
+{
+    internal class Songs
+    {
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public int Produced { get; set; }
+
+        public Songs(string title, string artist, int produced)
+        {
+            Title = title;
+            Artist = artist;
+            Produced = produced;
+        }
+
+        public virtual void DisplayInfo()
+        {
+            Console.WriteLine($"Title: {Title}");
+            Console.WriteLine($"Artist: {Artist}");
+            Console.WriteLine($"Year Produced: {Produced}");
+        }
+    }
+
+    class PopMusic : Songs
+    {
+        public string Language { get; set; }
+
+        public PopMusic(string title, string artist, int produced, string language)
+            : base(title, artist, produced)
+        {
+            Language = language;
+        }
+
+        public override void DisplayInfo()
+        {
+            Console.WriteLine("\n--- Pop Music ---");
+            Console.WriteLine($"Language: {Language}");
+            base.DisplayInfo();
+        }
+    }
+
+    class RockMusic : Songs
+    {
+        public string Instrument { get; set; }
+
+        public RockMusic(string title, string artist, int produced, string instrument)
+            : base(title, artist, produced)
+        {
+            Instrument = instrument;
+        }
+
+        public override void DisplayInfo()
+        {
+            Console.WriteLine("\n--- Rock Music ---");
+            Console.WriteLine($"Instrument: {Instrument}");
+            base.DisplayInfo();
+        }
+    }
+}
---
+++ YAGONIA_ACTIVITY 8/YAGONIA_ACTIVITY 8/UserInput.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YAGONIA_ACTIVITY_8
+{
+    internal class UserInput
+    {
+        public static int GetInt(string message)
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.Write(message);
+                    return int.Parse(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Invalid input. Please enter a number.");
+                    Console.ResetColor();
+                }
+            }
+        }
+
+        public static string GetString(string message)
+        {
+            Console.Write(message);
+            return Console.ReadLine();
+        }
+
+    }
+}
